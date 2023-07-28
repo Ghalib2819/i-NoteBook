a25534ad4171754a7497354c6f634d1620b05101
Ghalib2819 <126101570+Ghalib2819@users.noreply.github.com>
2023-07-28 06:59:19 -0700

    video 49 completed

--- backend/index.js
+++ backend/index.js
@@ -3,13 +3,13 @@ const express = require('express')
 connectToMongo();
 
 const app = express()
-const port = 3000
+const port = 5000
 
 app.use(express.json())
 
 //Available routes
 app.use('/api/auth', require('./routes/auth'))
-// app.use('/api/notes', require('./routes/notes'))
+app.use('/api/notes', require('./routes/notes'))
 
 
 app.get('/', (req, res) => {
--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,18 +1,21 @@
-const express = require('express');
-const { body, validationResult } = require('express-validator');
-const User = require('../models/User');
+const express = require("express");
+const { body, validationResult } = require("express-validator");
+const User = require("../models/User");
 const router = express.Router();
-
+const bcrypt = require('bcryptjs');
+var jwt = require('jsonwebtoken');
+const JWT_SECRET = 'ghalibisagoodboy';
 // Add express.json() middleware to parse incoming JSON data
 router.use(express.json());
 
 // POST request to save user data
+//create a user post "/api/auth/createuser"
 router.post(
-  '/',
+  "/createuser",
   [
-    body('name').isLength({ min: 3 }),
-    body('email').isEmail(),
-    body('password').isLength({ min: 3 }),
+    body("name").isLength({ min: 3 }),
+    body("email").isEmail(),
+    body("password").isLength({ min: 3 }),
   ],
   async (req, res) => {
     try {
@@ -28,19 +31,30 @@ router.post(
       const existingUser = await User.findOne({ email });
 
       if (existingUser) {
-        return res.status(400).json({ error: 'Email already exists' });
+        return res.status(400).json({ error: "Email already exists" });
       }
+     const salt = await bcrypt.genSalt(10);
+      secPass = await bcrypt.hash (req.body.password, salt);
+  
+user = await User.create({
+  name: req.body.name,
+  password: secPass,
+  email: req.body.email,
+});
 
-      // Create a new User instance
-      const user = new User({ name, email, password });
+const data = {
+  user:{
+    id: user.id
+  }
+}
+const authtoken = jwt.sign(data, JWT_SECRET);
 
-      // Save the user data to the database
-      await user.save();
-
-      res.send(req.body);
-    } catch (error) {
-      console.error('Error saving user data:', error.message);
-      res.status(500).send('Error saving user data');
+// res.json(user)
+res.json({authtoken})
+    }
+     catch (error) {
+      console.error("Error saving user data:", error.message);
+      res.status(500).send(req.body);
     }
   }
 );
